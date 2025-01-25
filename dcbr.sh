c3540b06e2cdf9e17df51f4ea2441d9e70e74132
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2025-01-25 13:27:08 +0100

    Add files via upload

---
+++ dcbr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+current_brightness=$(xrandr --verbose | grep -i brightness | awk '{print $2}')
+
+new_brightness=$(echo "$current_brightness - 0.1" | bc)
+
+if (( $(echo "$new_brightness > 1.0" | bc -l) )); then
+    new_brightness=1.0
+fi
+
+xrandr --output LVDS-1 --brightness $new_brightness
