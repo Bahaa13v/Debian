b5a3ec73ea4609235813022f965c8320cb157ed0
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2025-01-25 09:22:44 +0100

    Update Debian.sh

--- Debian.sh
+++ Debian.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+cp .bashrc /home/bahaa/.bashrc
+
+sudo apt install nala -y
+sudo nala updat
+sudo nala install xorg i3 alacritty fonts-noto
+
+mkdir -p /home/bahaa/.config/alacritty
+mkdir -p /home/bahaa/.config/i3
+
+sudo cp i3status.conf /etc/
+cp config /home/bahaa/.config/i3/
+cp alacritty.yml /home/bahaa/.config/alacritty/
